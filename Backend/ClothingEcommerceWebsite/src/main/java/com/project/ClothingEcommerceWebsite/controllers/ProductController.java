2a7cb1d356efdd76b68e43a283e5573adedad784
tuyenc211 <hoductuyenqp@gmail.com>
2025-09-16 09:44:46 +0700

    mm

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/controllers/ProductController.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/controllers/ProductController.java
@@ -1,4 +1,16 @@
 package com.project.ClothingEcommerceWebsite.controllers;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("${api.prefix}/products")
+@RequiredArgsConstructor
 public class ProductController {
+    @PostMapping("")
+    public ResponseEntity<?> createProduct() {
+        return ResponseEntity.ok().body("");
+    }
 }
