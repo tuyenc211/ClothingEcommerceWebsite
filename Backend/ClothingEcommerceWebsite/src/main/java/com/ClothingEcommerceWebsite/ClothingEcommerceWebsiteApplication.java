1ef37afc6c0955b630d10cc5fc4f16aacbab66ab
tuyenc211 <hoductuyenqp@gmail.com>
2025-09-16 09:27:43 +0700

    first commit

---
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.ClothingEcommerceWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingEcommerceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingEcommerceWebsiteApplication.class, args);
+	}
+
+}
