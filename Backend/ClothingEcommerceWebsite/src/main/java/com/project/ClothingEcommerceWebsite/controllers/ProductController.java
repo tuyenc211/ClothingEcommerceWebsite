08752fabc9a4254a4abed80111707fb5c437abb1
tuyenc211 <hoductuyenqp@gmail.com>
2025-09-30 15:52:08 +0700

    mm

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/controllers/ProductController.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/controllers/ProductController.java
@@ -1,9 +1,8 @@
 package com.project.ClothingEcommerceWebsite.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("${api.prefix}/products")
@@ -11,6 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     @PostMapping("")
     public ResponseEntity<?> createProduct() {
+        return ResponseEntity.ok().body("");
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable Long id) {
+        return ResponseEntity.ok().body("");
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCategory() {
+        return ResponseEntity.ok().body("");
+    }
+    @DeleteMapping("")
+    public ResponseEntity<?> deleteProduct() {
         return ResponseEntity.ok().body("");
     }
 }
