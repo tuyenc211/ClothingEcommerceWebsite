f3da0d738b3740cc0638c6db1532624518cc01ad
tuyenc211 <hoductuyenqp@gmail.com>
2025-10-16 16:58:31 +0700

    mm

---
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/models/ProductVariant.java
@@ -0,0 +1,35 @@
+package com.project.ClothingEcommerceWebsite.models;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product_variants")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductVariant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "size_id")
+    private Size size;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "color_id")
+    private Color color;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String sku;
+
+    private Double price;
+}
