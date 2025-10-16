9a9dce53c3a1c98eefd9e91d4aba21fd7394250a
tuyenc211 <hoductuyenqp@gmail.com>
2025-10-16 16:04:27 +0700

    mm

---
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/configs/CloudinaryConfig.java
@@ -0,0 +1,19 @@
+package com.project.ClothingEcommerceWebsite.configs;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CloudinaryConfig {
+    @Bean
+    public Cloudinary cloudinary() {
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "Root",     // 🔹 thay bằng của bạn
+                "api_key", "794879747219491",
+                "api_secret", "FWuxg7Zf3kg9uwonLtBQ5AbeYqY",
+                "secure", true
+        ));
+    }
+}
---
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/models/Product.java
@@ -0,0 +1,40 @@
+package com.project.ClothingEcommerceWebsite.models;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String sku;
+
+    @Column(nullable = false, unique = true, length = 280)
+    private String slug;
+
+    @Column(nullable = false, length = 255)
+    private String name;
+
+    @Column(columnDefinition = "MEDIUMTEXT")
+    private String description;
+
+    @Column(name = "base_price", nullable = false)
+    private Double basePrice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "is_published", nullable = false)
+    private Boolean isPublished = true;
+}
