4aa1020b3f7d3ae530c1dcf88c31b4a3373d3f32
JustTaiCorn <taicorn2004@gmail.com>
2025-10-16 16:43:12 +0700

    refactor: Remove unnecessary auth checks in ResetPasswordPage component

--- Frontend/dashboard-admin/src/middleware.ts
+++ Frontend/dashboard-admin/src/middleware.ts
@@ -24,5 +24,5 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
-  matcher: ["/", "/admin/:path*"],
+  matcher: ["/", "/admin/:path*", "/login"],
 };
