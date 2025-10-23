8cd8a4369df03dd75997f0d87458781cdeb6a9f0
tuyenc211 <hoductuyenqp@gmail.com>
2025-10-23 16:07:47 +0700

    mm

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/services/impl/ProductImageServiceImpl.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/services/impl/ProductImageServiceImpl.java
@@ -19,20 +19,26 @@ public class ProductImageServiceImpl implements ProductImageService {
     private final ProductImageRepository imageRepository;
     private final CloudinaryService cloudinaryService;
     private final ProductRepository productRepository;
+
     @Override
     public List<ProductImage> uploadAndSaveImages(List<MultipartFile> files, Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new RuntimeException("Product not found"));
+        List<ProductImage> existingImages = imageRepository.findAllByProductId(productId);
+        int maxPosition = existingImages.stream()
+                .mapToInt(ProductImage::getPosition)
+                .max()
+                .orElse(0);
         List<ProductImage> savedImages = new ArrayList<>();
+        int currentPosition = maxPosition;
         for (MultipartFile file : files) {
+            currentPosition++;
             String imageUrl = cloudinaryService.uploadImage(file);
-
             ProductImage productImage = ProductImage.builder()
                     .imageUrl(imageUrl)
                     .product(product)
-                    .position(1)
+                    .position(currentPosition)
                     .build();
-
             savedImages.add(imageRepository.save(productImage));
         }
         return savedImages;
