01dd8392f512751de827df0de4eeffc826a95eeb
JustTaiCorn <taicorn2004@gmail.com>
2025-11-03 10:43:45 +0700

    feat: Enhance Toaster component with rich colors and custom toast options

--- Frontend/client-website/src/app/layout.tsx
+++ Frontend/client-website/src/app/layout.tsx
@@ -36,7 +36,9 @@ export default function RootLayout({
           <Footer />
         </div>
         <ScrollToTopAndContactButton />
-        <Toaster />
+        <Toaster richColors={true} toastOptions={ {
+            style: { borderRadius: '0px', boxShadow: "2px 2px 0px rgba(0,0,0,1), 5px 5px 0px rgba(120,120,120,1" }
+        }}/>
       </body>
     </html>
   );
--- Frontend/dashboard-admin/src/app/layout.tsx
+++ Frontend/dashboard-admin/src/app/layout.tsx
@@ -4,13 +4,12 @@ import { Metadata } from "next";
 import "@/app/globals.css";
 import { Toaster } from "sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import {Geist, Geist_Mono} from "next/dist/compiled/@next/font/dist/google";
 
 // pages have to be rendered dynamically because supabase server component client uses cookies
 export const dynamic = "force-dynamic";
 export const metadata: Metadata = {
   title: {
-    template: "%s - Aristino",
+    template: "%s - Atino Admin Dashboard",
     default: "Atino Admin Dashboard",
   },
 };
@@ -25,7 +24,12 @@ export default function RootLayout({
       <body suppressHydrationWarning >
         {/* <TanstackQueryProvider > */}
         <TooltipProvider>{children}</TooltipProvider>
-        <Toaster />
+        <Toaster  richColors={true} toastOptions={
+            {
+             style: { borderRadius: '0px', boxShadow: "2px 2px 0px rgba(0,0,0,1), 5px 5px 0px rgba(120,120,120,1" }
+            }
+        }
+        />
         {/* </TanstackQueryProvider> */}
       </body>
     </html>
