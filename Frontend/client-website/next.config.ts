93dadcba44cd12db385678c72d43d40ddc373b9f
JustTaiCorn <taicorn2004@gmail.com>
2025-11-05 20:13:16 +0700

    hmm

--- Frontend/client-website/next.config.ts
+++ Frontend/client-website/next.config.ts
@@ -13,34 +13,34 @@ const nextConfig: NextConfig = {
         hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
       {
         protocol: "https",
         hostname: "picsum.photos",
       },
-        {
-            protocol: "https",
-            hostname: "pos.nvncdn.com",
-        },
+      {
+        protocol: "https",
+        hostname: "pos.nvncdn.com",
+      },
     ],
-  }, 
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: "/address-kit/:path*",
-  //       destination: "https://production.cas.so/address-kit/:path*",
-  //     },
-  //     {
-  //       source: "/api/v1/:path*",
-  //       destination:
-  //         "https://clothingecommercewebsite.onrender.com/api/v1/:path*",
-  //     },
-  //   ];
-  // },
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/address-kit/:path*",
+        destination: "https://production.cas.so/address-kit/:path*",
+      },
+      {
+        source: "/api/v1/:path*",
+        destination:
+          "https://clothingecommercewebsite.onrender.com/api/v1/:path*",
+      },
+    ];
+  },
   // rewrites: async () => [
   //   {
   //     source: "/api/:path*",
