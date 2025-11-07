cf42b16e76e2e5a412eab422a541e3b138e92783
tuyenc211 <hoductuyenqp@gmail.com>
2025-11-07 21:08:21 +0700

    mm

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
@@ -1,11 +1,14 @@
 package com.project.ClothingEcommerceWebsite;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.parameters.P;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ClothingEcommerceWebsiteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ClothingEcommerceWebsiteApplication.class, args);
 	}
