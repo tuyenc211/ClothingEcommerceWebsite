30ff29de5b8c05baedc05054b3debf2234ba1011
tuyenc211 <hoductuyenqp@gmail.com>
2025-11-09 20:28:27 +0700

    mm

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ClothingEcommerceWebsiteApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(ClothingEcommerceWebsiteApplication.class, args);
 	}
 
