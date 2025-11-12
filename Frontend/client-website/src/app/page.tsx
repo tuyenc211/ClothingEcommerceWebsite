1eaf4a496b5bc60cc6a0a5dc81befba07b66683a
JustTaiCorn <taicorn2004@gmail.com>
2025-11-12 11:04:05 +0700

    hmm

--- Frontend/client-website/src/app/page.tsx
+++ Frontend/client-website/src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
   const { fetchProducts } = useProductStore();
   const {fetchColors} = useColorStore();
   const {fetchSizes} = useSizeStore();
+
   useEffect(() => {
     fetchProducts()
     fetchColors();
