735327b541c868cda90fc596558abe3f9a4b0a86
tuyenc211 <hoductuyenqp@gmail.com>
2025-11-23 02:21:15 +0700

    a

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/repositories/ProductImageRepository.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/repositories/ProductImageRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
     List<ProductImage> findAllByProductId(Long productId);
     void deleteAllByProductId(Long productId);
+    List<ProductImage> findAllByProductIdIn(List<Long> productIds);
 }
