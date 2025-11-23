f1627a7b89dafb7c5c83e361bfb8233cbfa36ea0
JustTaiCorn <taicorn2004@gmail.com>
2025-11-23 15:31:00 +0700

    Refactor React Query integration: create ReactQueryProvider component and update RootLayout to use it

--- Frontend/client-website/src/app/layout.tsx
+++ Frontend/client-website/src/app/layout.tsx
@@ -4,12 +4,8 @@ import "./globals.css";
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
 import { Toaster } from "sonner";
-
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "@/lib/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import ScrollToTopAndContactButton from "@/components/common/ScrollToTop";
+import ReactQueryProvider from "@/providers/ReactQueryProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -36,7 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        <QueryClientProvider client={queryClient}>
+        <ReactQueryProvider>
           <div className="min-h-screen flex flex-col">
             <Header />
             <main className="flex-1">{children}</main>
@@ -44,8 +40,7 @@ export default function RootLayout({
           </div>
           <ScrollToTopAndContactButton />
           <Toaster richColors={true} />
-          <ReactQueryDevtools initialIsOpen={true} />
-        </QueryClientProvider>
+        </ReactQueryProvider>
       </body>
     </html>
   );
