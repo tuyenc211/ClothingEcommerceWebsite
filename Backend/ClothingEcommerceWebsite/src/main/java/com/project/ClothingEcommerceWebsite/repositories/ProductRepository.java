b17582b499223cbf6fe954ac9751cec72b604bc6
tuyenc211 <hoductuyenqp@gmail.com>
2025-12-25 01:11:48 +0700

    a

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/repositories/ProductRepository.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/repositories/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.project.ClothingEcommerceWebsite.repositories;
 
 import com.project.ClothingEcommerceWebsite.models.Product;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     boolean existsBySku(String sku);
     List<Product> findByNameContainingIgnoreCase(String name);
     boolean existsBySkuAndIdNot(String sku, Long id);
