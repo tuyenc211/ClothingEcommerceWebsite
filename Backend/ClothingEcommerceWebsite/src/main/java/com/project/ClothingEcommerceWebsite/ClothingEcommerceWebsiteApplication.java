bdb07972b61dd9a21e752bd56b72fe803263a402
tuyenc211 <hoductuyenqp@gmail.com>
2025-12-26 15:44:48 +0700

    UPDATE PAGE

--- Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
+++ Backend/ClothingEcommerceWebsite/src/main/java/com/project/ClothingEcommerceWebsite/ClothingEcommerceWebsiteApplication.java
@@ -1,5 +1,6 @@
 package com.project.ClothingEcommerceWebsite;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,15 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 public class ClothingEcommerceWebsiteApplication {
+	static {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(e ->
+				System.setProperty(e.getKey(), e.getValue())
+		);
+	}
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(ClothingEcommerceWebsiteApplication.class, args);
